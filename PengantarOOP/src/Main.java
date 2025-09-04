2baab0b0cc153afc40c7ea23225811f14d8ead90
sintyakd <126735029+sintya23@users.noreply.github.com>
2025-09-04 21:16:57 +0800

    Main java

--- PengantarOOP/src/Main.java
+++ PengantarOOP/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+// Membuat objek Scanner untuk menerima input dari keyboard
+        Scanner scanner = new Scanner(System.in);
+// Meminta pengguna memasukkan nama
+        System.out.print("Masukkan nama Anda: ");
+        String nama = scanner.nextLine();
+// Menampilkan pesan sambutan yang dipersonalisasi
+        System.out.println("Selamat Datang di Java, " + nama + "!");
+// Menutup objek scanner
+        scanner.close();
     }
 }
